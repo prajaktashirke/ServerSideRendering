e26eafa9b2831da0bc22d209b1aa5b82035c3501
prajaktashirke <prajaktashirke20@gmail.com>
2023-01-07 19:03:15 +0530

    set up code added

---
+++ src/client/Components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+    return <div> I am on home page</div>
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const React = require("react");
+const renderToString = require("react-dom/server").renderToString;
+
+const Home = require("./client/Components/Home").default;
+
+app.get("/", (req,res) => {
+    const content = renderToString(<Home />)
+    res.send(content);
+});
+
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+})
